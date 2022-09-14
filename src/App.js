d3b128a1416ad7504ccf4d4bfd7ae6f621f664c5
redpumpkin21 <christopher.johnson2108@gmail.com>
2022-09-14 13:05:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  const url = "https://glamorous-newt-sundress.cyclic.app/todo"
+  const [todo, setTodo] = useState([])
+  const nullTodo = {
+    title: "",
+    description: "",
+  }
+  const [targetTodo, setTargetTodo] = useState(nullTodo)
+  const getTodo = async() => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setTodo(data)
+  }
+
+useEffect (() => {getTodo()}, [])
+
+const addTodo = async(newTodo) => {
+  const response = await fetch(url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newTodo),
+  })
+  getTodo()
+}
+
+const getTargetTodo = (todo) => {
+  setTargetTodo(todo)
+  props.history.push("/edit")
+}
+
+const updateTodo = async (todo) => {
+  const responce = await fetch(url + todo.id + "/", {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(todo)
+  })
+  getTodo()
+}
+
+const deleteTodo = async (todo) => {
+  const response = await fetch(url + todo.id + "/", {
+    method: "delete",
+  })
+  getTodo()
+  props.history.push("/")
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Hey</h1>
       </header>
     </div>
   );
---
+++ src/Compnents/Todo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Todo= () => {
+    return(
+        <div>
+           <h3>Title</h3>
+           <p>description</p>
+        </div>
+    )
+}
